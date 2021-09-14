86b983c2664125d5bc9b85eda82e299d6db7ab28
Gábor Stuksza <gabor.stuksza@attrecto.com>
2021-09-14 14:34:23 +0200

    Movie list with onClick toast

--- app/src/main/java/com/attrecto/academy/android/MainActivity.kt
+++ app/src/main/java/com/attrecto/academy/android/MainActivity.kt
@@ -2,15 +2,20 @@ package com.attrecto.academy.android
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -23,7 +28,7 @@ class MainActivity : ComponentActivity() {
             AttrectoAcademyAndroidTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MovieCard(FakeData.movies[0])
+                    MovieList(FakeData.movies)
                 }
             }
         }
@@ -32,12 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MovieCard(movie: Movie) {
+    val context = LocalContext.current
+
     Surface(
         shape = MaterialTheme.shapes.medium,
         elevation = 6.dp,
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
+            .clickable {
+                Toast
+                    .makeText(context, "Click", Toast.LENGTH_SHORT)
+                    .show()
+            }
     ) {
         Row(modifier = Modifier.padding(all = 8.dp)) {
             Image(
@@ -68,6 +79,18 @@ fun MovieCard(movie: Movie) {
                     style = MaterialTheme.typography.subtitle1
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun MovieList(movies: List<Movie>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(all = 8.dp)
+    ) {
+        items(movies) {
+            MovieCard(movie = it)
         }
     }
 }
