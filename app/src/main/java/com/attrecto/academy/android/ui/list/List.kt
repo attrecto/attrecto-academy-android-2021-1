66a0512148357da6b33a414746bb4662e4eddcfd
Gábor Stuksza <gabor.stuksza@attrecto.com>
2021-09-14 15:34:30 +0200

    Scaffold, toolbar, dropdown menu, sorting

--- app/src/main/java/com/attrecto/academy/android/ui/list/List.kt
+++ app/src/main/java/com/attrecto/academy/android/ui/list/List.kt
@@ -6,24 +6,97 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.attrecto.academy.android.FakeData
+import com.attrecto.academy.android.R
 import com.attrecto.academy.android.model.Movie
 import com.attrecto.academy.android.ui.main.Screen
 import com.attrecto.academy.android.ui.theme.AttrectoAcademyAndroidTheme
 
 @Composable
 fun ListScreen(navController: NavController) {
-    MovieList(FakeData.movies) {
-        navController.navigate(Screen.Detail.route(it))
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+
+    val movies = remember {
+        mutableStateOf(FakeData.movies)
+    }
+
+    val titleRes = remember {
+        mutableStateOf(R.string.app_bar_title)
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = titleRes.value))
+                },
+                actions = {
+                    Box(
+                        Modifier.wrapContentSize(Alignment.TopEnd)
+                    ) {
+                        IconButton(onClick = {
+                            expanded.value = true
+                        }) {
+                            Icon(
+                                Icons.Filled.MoreVert,
+                                contentDescription = null
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = expanded.value,
+                            onDismissRequest = { expanded.value = false }
+                        ) {
+                            DropdownMenuItem(onClick = {
+                                expanded.value = false
+                                titleRes.value = R.string.sort_by_title
+                                movies.value = movies.value.sortedBy { it.title }
+                            }) {
+                                Text(text = stringResource(R.string.sort_by_title))
+                            }
+
+                            Divider()
+
+                            DropdownMenuItem(onClick = {
+                                expanded.value = false
+                                titleRes.value = R.string.sort_by_year
+                                movies.value = movies.value.sortedBy { it.year }
+                            }) {
+                                Text(text = stringResource(R.string.sort_by_year))
+                            }
+
+                            Divider()
+
+                            DropdownMenuItem(onClick = {
+                                expanded.value = false
+                                titleRes.value = R.string.sort_by_imdb_rating
+                                movies.value = movies.value.sortedBy { it.imdbRating }.reversed()
+                            }) {
+                                Text(text = stringResource(R.string.sort_by_imdb_rating))
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    ) {
+        MovieList(movies.value) {
+            navController.navigate(Screen.Detail.route(it))
+        }
     }
 }
 
@@ -63,7 +136,7 @@ fun MovieCard(movie: Movie, onMovieClick: (imdbId: String) -> Unit) {
                 Spacer(modifier = Modifier.height(4.dp))
 
                 Text(
-                    text = movie.imdbId,
+                    text = "${movie.imdbRating}",
                     style = MaterialTheme.typography.subtitle1
                 )
             }
