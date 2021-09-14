931c873a98c96f89e8600ae0826581b34826fd86
Gábor Stuksza <gabor.stuksza@attrecto.com>
2021-09-14 13:58:16 +0200

    Movie card

--- app/src/main/java/com/attrecto/academy/android/MainActivity.kt
+++ app/src/main/java/com/attrecto/academy/android/MainActivity.kt
@@ -3,12 +3,16 @@ package com.attrecto.academy.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.attrecto.academy.android.ui.theme.AttrectoAcademyAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,9 +31,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MovieCard(movie: Movie) {
-    Column {
-        Text(movie.title)
-        Text(movie.year.toString())
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        elevation = 6.dp,
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = rememberImagePainter(data = movie.poster),
+                contentDescription = null,
+                modifier = Modifier.size(90.dp, 120.dp)
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Column {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.h5
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = "${movie.year}",
+                    style = MaterialTheme.typography.subtitle1
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = movie.imdbId,
+                    style = MaterialTheme.typography.subtitle1
+                )
+            }
+        }
     }
 }
 
