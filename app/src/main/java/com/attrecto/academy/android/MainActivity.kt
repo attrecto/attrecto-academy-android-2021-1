962c39a2335dcc2dce8e2612c871c102c2705c63
Balázs Dániel <balazs.daniel@attrecto.com>
2021-10-21 08:26:24 +0200

    created list

--- app/src/main/java/com/attrecto/academy/android/MainActivity.kt
+++ app/src/main/java/com/attrecto/academy/android/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.attrecto.academy.android
 
+import android.content.Context
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -23,7 +39,7 @@ class MainActivity : ComponentActivity() {
             AttrectoAcademyAndroidTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MovieCard(FakeData.movies[0])
+                    ListScreen(FakeData.movies)
                 }
             }
         }
@@ -32,12 +48,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MovieCard(movie: Movie) {
+    val context = LocalContext.current
+
     Surface(
         shape = MaterialTheme.shapes.medium,
         elevation = 6.dp,
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
+            .clickable {
+                Toast
+                    .makeText(context, "movie.title", Toast.LENGTH_SHORT)
+                    .show()
+                Log.e("onclick", "movie click")
+            }
     ) {
         Row(modifier = Modifier.padding(all = 8.dp)) {
             Image(
@@ -68,6 +91,19 @@ fun MovieCard(movie: Movie) {
                     style = MaterialTheme.typography.subtitle1
                 )
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ListScreen(movies: List<Movie> = FakeData.movies) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(movies) {
+            MovieCard(movie = it)
         }
     }
 }
